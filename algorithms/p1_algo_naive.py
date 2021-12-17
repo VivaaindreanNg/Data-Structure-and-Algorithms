6d3dd66a96d05eb2d2a9a3a7f53c65e358e2d3bf
VivaaindreanNg <123ulugubang@gmail.com>
2021-12-17 20:55:01 +0800

    Implement sliding window technique as naive approach

--- algorithms/p1_algo_naive.py
+++ algorithms/p1_algo_naive.py
@@ -1,7 +1,30 @@
 
 
-def solve(input_str: str) -> list:
+def solve(input_str: str, pattern: str) -> list:
+    """
+    Using sliding window technique to slide across every single
+    input char in input_str with size n. 
+    The length of the window/pattern is m, assuming m <= n.
+
+    Time complexity: O(n*m)
+    """
     output = []
+
+    window_length = len(pattern)
+    input_str = input_str.rstrip().lstrip()
+
+    for idx, v in enumerate(input_str):
+        start = idx 
+        end = idx+window_length
+
+        tmp = ""
+
+        if end <= len(input_str):
+            for x in range(start, end):
+                tmp += input_str[x]
+
+        if tmp == pattern:
+            output.append(start)
 
     return output 
 
@@ -9,5 +32,6 @@ if __name__ == '__main__':
     input = """\
     AABAACAADAABAABA
     """
+    pattern = "AABA"
 
-    print(solve(input))
+    print(solve(input, pattern))
