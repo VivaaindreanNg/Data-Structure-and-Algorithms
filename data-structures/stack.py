f600cd62132275dd95e0bffe6cfc5b87f823e4f6
VivaaindreanNg <123ulugubang@gmail.com>
2021-12-22 05:41:14 +0800

    Added remaining core functions for Stack

--- data-structures/stack.py
+++ data-structures/stack.py
@@ -8,14 +8,25 @@ class Stack:
     def __str__(self) -> str:
         return str([i for i in self.input_list])
 
-    def push(self, num: int) -> list:
+    def push(self, element: Any) -> list:
         pass 
 
     def pop(self) -> Any:
         pass
 
+    def get_top(self) -> Any:
+        """
+        Returns top of the element within the stack.
+        """
+        pass
+
+    def is_empty(self) -> bool:
+        return True if len(self.input_list) == 0 else False
+
 
 if __name__ == '__main__':
-    stack = Stack([1, 2, 3, 4])
+    stack = Stack([])
 
     print(stack)
+
+    print(f"Check if stack is empty: {stack.is_empty()}")
