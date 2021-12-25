bcead251ce83f926c005ada4efdb17a6efa7bb8e
VivaaindreanNg <123ulugubang@gmail.com>
2021-12-25 13:40:13 +0800

    Updated test module with unittest

--- data_structures/tests/test_stack.py
+++ data_structures/tests/test_stack.py
@@ -10,7 +10,6 @@ class TestStack(unittest.TestCase):
 
         stack.push(1)
         stack.push(2)
-
         self.assertEqual(len(stack.input_list), 2)
 
     def test_pop(self) -> None:
@@ -18,12 +17,10 @@ class TestStack(unittest.TestCase):
         stack.push(1)
         stack.push(2)
         stack.push(3)
-
         self.assertEqual(len(stack.input_list), 3)
 
         stack.pop()
         stack.pop()
-
         self.assertEqual(len(stack.input_list), 1)
 
     def test_get_top(self) -> None:
@@ -31,21 +28,14 @@ class TestStack(unittest.TestCase):
         stack.push(1)
         stack.push(2)
         stack.push(3)
-
         self.assertEqual(stack.get_top(), 3)
 
     def test_is_empty(self) -> None:
         stack = Stack()
         stack.push(1)
         stack.push(2)
-
         self.assertFalse(stack.is_empty())
 
         stack.pop()
         stack.pop()
-
         self.assertTrue(stack.is_empty())
-
-
-if __name__ == "___main__":
-    unittest.main()
