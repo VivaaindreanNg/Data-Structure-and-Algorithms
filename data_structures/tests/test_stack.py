868c9d16fafc0280015e82d0ac2312241e9c8c7f
VivaaindreanNg <123ulugubang@gmail.com>
2021-12-25 13:29:23 +0800

    Updated test module with unittest

--- data_structures/tests/test_stack.py
+++ data_structures/tests/test_stack.py
@@ -6,22 +6,45 @@ from ..src.stack import Stack
 class TestStack(unittest.TestCase):
     def test_push(self) -> None:
         stack = Stack()
-        assert len(stack.input_list) == 0
+        self.assertEqual(len(stack.input_list), 0)
 
         stack.push(1)
         stack.push(2)
 
-        assert len(stack.input_list) == 2
+        self.assertEqual(len(stack.input_list), 2)
 
     def test_pop(self) -> None:
+        stack = Stack()
+        stack.push(1)
+        stack.push(2)
+        stack.push(3)
 
-        pass
+        self.assertEqual(len(stack.input_list), 3)
+
+        stack.pop()
+        stack.pop()
+
+        self.assertEqual(len(stack.input_list), 1)
 
     def test_get_top(self) -> None:
-        pass
+        stack = Stack()
+        stack.push(1)
+        stack.push(2)
+        stack.push(3)
+
+        self.assertEqual(stack.get_top(), 3)
 
     def test_is_empty(self) -> None:
-        pass
+        stack = Stack()
+        stack.push(1)
+        stack.push(2)
+
+        self.assertFalse(stack.is_empty())
+
+        stack.pop()
+        stack.pop()
+
+        self.assertTrue(stack.is_empty())
 
 
 if __name__ == "___main__":
